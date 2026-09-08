2625447f3004bd25b08656736e75d7ed9d8ebaeb
Henrique Bignardi <hen.big.otte@gmail.com>
2026-09-08 14:39:31 -0300

    feat(C): adiciona exercicio 1.2 do K&R

---
+++ C/kr_book/cap1/Ex1_02_escape.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("Quebra de linha:\nLinha nova\n\n");
+    printf("Tabulacao:\tColuna A\tColuna B\n");
+    printf("Caracteres especiais: \"Aspas\" e \\Barra\\\n");
+    return 0;
+}
